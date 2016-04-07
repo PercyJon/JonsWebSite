bd9e42fc9e6ace67b6e683305a8f49e27e9b0a43
PercyJon <PercyJon.H@gmail.com>
2016-04-07 16:37:07 +0800

    create the login page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,6 +34,14 @@ Route::group(['namespace' => 'Admin', 'middleware' => 'auth'],function(){
 });
 
 
-Route::get('/auth/login', 'Auth\AuthController@getLogin');
-Route::post('/auth/login', 'Auth\AuthController@postLogin');
-Route::get('/auth/logout', 'Auth\AuthController@getLogout');
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
