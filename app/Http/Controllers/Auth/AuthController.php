db92cb9ec5c42732d439a2fee812e120e5b3dc6f
PercyJon <PercyJon.H@gmail.com>
2016-04-11 10:23:30 +0800

    Revert "change the login redirect page."
    This reverts commit 0c09cbeb58e5106c0e2d59635a78263c49f9da50.
    
    # Conflicts:
    #	.idea/workspace.xml
    #	app/Http/Controllers/BlogController.php
    #	app/Http/routes.php
    #	resources/views/blog/articlecontent.blade.php
    #	resources/views/blog/articles.blade.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,8 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectAfterLogout = '/login';
-    protected $redirectTo = '/admin/post';
+    protected $redirectTo = '/';
 
     /**
      * Create a new authentication controller instance.
