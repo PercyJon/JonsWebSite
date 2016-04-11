9b672d85c1caa995ba6a64fffbc934fd0ebdf2aa
PercyJon <PercyJon.H@gmail.com>
2016-04-11 10:26:39 +0800

    cc

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,1 +1,23 @@
 <?php
+
+// Blog pages
+Route::get('/', function () {
+    return redirect('/');
+});
+Route::get('blog', 'BlogController@index');
+Route::get('blog/{slug}', 'BlogController@showPost');
+
+// Admin area
+Route::get('admin', function () {
+    return redirect('/admin/post');
+});
+Route::$router->group(['namespace' => 'Admin', 'middleware' => 'auth'], function () {
+    resource('admin/post', 'PostController');
+    resource('admin/tag', 'TagController');
+    get('admin/upload', 'UploadController@index');
+});
+
+// Logging in and out
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
