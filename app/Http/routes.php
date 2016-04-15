2ea149dd04befb476c56615969377c52dd04e13c
PercyJon <PercyJon.H@gmail.com>
2016-04-15 23:37:48 +0800

    .

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,7 +18,7 @@ Route::group(['namespace' => 'Admin'], function () {
 });
 
 Route::group(['middleware' => 'web'],function(){
-    Route::auth();
+    Route::auth();  //todo
     Route::get('/auth/login', 'Auth\AuthController@getLogin');
     Route::post('/auth/login', 'Auth\AuthController@postLogin');
     Route::get('/auth/logout', 'Auth\AuthController@getLogout');
